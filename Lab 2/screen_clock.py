0a57a03a38824ec8a73a7b77128181a20a99c468
Chelsea <cl773@cornell.edu>
2021-02-27 16:19:40 -0800

    update

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -75,6 +75,20 @@ while True:
     #TODO: fill in here. You should be able to look in cli_clock.py and stats.py 
 
     image = Image.open("red.jpg")
+    image_ratio = image.width / image.height
+    screen_ratio = width / height
+    if screen_ratio < image_ratio:
+        scaled_width = image.width * height // image.height
+        scaled_height = height
+    else:
+        scaled_width = width
+        scaled_height = image.height * width // image.width
+    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
+
+    # Crop and center the image
+    x = scaled_width // 2 - width // 2
+    y = scaled_height // 2 - height // 2
+    image = image.crop((x, y, x + width, y + height))
 
 
     clocktime = strftime("%H:%M:%S")
@@ -94,6 +108,8 @@ while True:
     if buttonA.value and not buttonB.value:
         # disp.fill(red)
         draw.text((x_3, y_3), dateTime, font=fontForTimeOfWeek, fill="#FFFFFF")
+        disp.image(image, rotation)
+
 
     else: 
         draw.text((x_1, y_1), clocktime, font=fontForTimeExact, fill="#FFFFFF")
@@ -101,20 +117,7 @@ while True:
         draw.text((x_2, y_2), dayWeek, font=fontForTimeOfWeek, fill="#FFFFFF")
 
 
-    image_ratio = image.width / image.height
-    screen_ratio = width / height
-    if screen_ratio < image_ratio:
-        scaled_width = image.width * height // image.height
-        scaled_height = height
-    else:
-        scaled_width = width
-        scaled_height = image.height * width // image.width
-    image = image.resize((scaled_width, scaled_height), Image.BICUBIC)
 
-    # Crop and center the image
-    x = scaled_width // 2 - width // 2
-    y = scaled_height // 2 - height // 2
-    image = image.crop((x, y, x + width, y + height))
 
     # y += font.getsize(IP)[1]
     # draw.text((x, y), WTTR, font=font, fill="#FFFF00")
@@ -124,5 +127,5 @@ while True:
     # draw.text((x, y), Temp, font=font, fill="#FF00FF")
 
     # Display image.
-    disp.image(image, rotation)
+    # disp.image(image, rotation)
     time.sleep(1)
