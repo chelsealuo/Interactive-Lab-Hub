68ec95098dcb64378928d0b9672f445ee4415358
Chelsea <cl773@cornell.edu>
2021-03-08 00:06:18 -0800

    commit

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -75,7 +75,7 @@ buttonB = digitalio.DigitalInOut(board.D24)
 # t = 25*60
 # restt = 5*60
 
-# tomatoimage = Image.open("tomato.png")
+tomatoimage = Image.open("tomato.png")
 # tomatoimage = tomatoimage.crop((0, 0, 50, 100))
 
 
